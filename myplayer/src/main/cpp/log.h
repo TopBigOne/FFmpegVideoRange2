297aa36893fa861c8fb40ecbcff1974c7168f280
TopJar <chinagyzhou@gmail.com>
2025-05-07 18:30:51 +0800

    1:使项目能跑起来；

--- myplayer/src/main/cpp/log.h
+++ myplayer/src/main/cpp/log.h
@@ -7,6 +7,6 @@
 
 #include <android/log.h>
 
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,"FFmpeg:",__VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,"Jar_FFmpeg:",__VA_ARGS__)
 
 #endif //MYFFMPEGPLAYERTEST1_LOG_H
